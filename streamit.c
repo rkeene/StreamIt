422f516d42dcc49b246eef2c020fdef24bd9840a
rkeene <rkeene@noemail.net>
2004-11-17 10:13:02 +0000

    Code cleanup
    FossilOrigin-Name: 357f6b2fb0a326c3e0597cc3173c3f42d9358e3be6838d901f104a26649a5f27

--- streamit.c
+++ streamit.c
@@ -40,7 +40,7 @@ extern int errno;
 #define DEBUG_LOG(y...) /**/ 
 #endif
 
-void sighandler(int sig) {
+void sigpipehandler(int sig) {
 	return;
 }
 
@@ -61,10 +61,20 @@ int main(int argc, char **argv) {
 	int block_backoff=1, block_backoff_total=1;
 	unsigned long backoff_mult=30, backoff_mult_incr=0;
 
-	if (argc<4) return(print_help());
-	if (strcmp(argv[1], "-p")!=0) return(print_help());
-	stream_port=atoi(argv[2]);
-	if (stream_port<=0) return(print_help());
+	if (argc < 4) {
+		return(print_help());
+	}
+
+	if (strcmp(argv[1], "-p") != 0) {
+		return(print_help());
+	}
+
+	/* Determine the port to listen on. */
+	stream_port = atoi(argv[2]);
+
+	if (stream_port <= 0) {
+		return(print_help());
+	}
 
 #ifndef DEBUG
 	if (fork()!=0) {
@@ -78,47 +88,62 @@ int main(int argc, char **argv) {
 	}
 #endif
 
-        signal(SIGPIPE, sighandler);
+        signal(SIGPIPE, sigpipehandler);
 	LOG_INIT("streamit", LOG_DAEMON);
 	LOG_MSG("Starting up.");
 
-	for (i=0;i<(sizeof(clients)/sizeof(int));i++) clients[i]=-1;
-	for (i=0;i<(sizeof(clients_http)/sizeof(int));i++) clients_http[i]=0;
+	for (i = 0; i < (sizeof(clients)/sizeof(int)); i++) {
+		clients[i] = -1;
+	}
+	for (i = 0; i < (sizeof(clients_http)/sizeof(int)); i++) {
+		clients_http[i] = 0;
+	}
 
-	while ((master_fd=createlisten(stream_port))<0) {
+	while ((master_fd = createlisten(stream_port)) < 0) {
 		LOG_MSG("Couldn't bind to socket.");
 		sleep(1);
 	}
-	sockflags=fcntl(master_fd, F_GETFL);
+	sockflags = fcntl(master_fd, F_GETFL);
 	fcntl(master_fd, F_SETFL, sockflags|O_NONBLOCK);
 
 	while (1) {
-		if (instr_buf_len<=0) {
-			if (argv[++curr_file]==NULL) curr_file=3;
-			if (instr_fd!=-1) { close(instr_fd); instr_fd=-1; }
-			if ((instr_fd=open(argv[curr_file], O_RDONLY))<0) {
+		if (instr_buf_len <= 0) {
+			if (argv[++curr_file] == NULL) {
+				curr_file=3;
+			}
+
+			if (instr_fd != -1) {
+				close(instr_fd);
+				instr_fd = -1;
+			}
+
+			if ((instr_fd = open(argv[curr_file], O_RDONLY)) < 0) {
 				continue;
 			}
 			LOG_MSG("Playing %s", argv[curr_file]);
 		}
-		instr_buf_len=read(instr_fd, buf, sizeof(buf));
-		if (instr_buf_len==0) continue;
+
+		instr_buf_len = read(instr_fd, buf, sizeof(buf));
+		if (instr_buf_len == 0) continue;
 
 		/* Accept incoming connections, if there are any. */
-		if ((sock_fd=accept(master_fd, (struct sockaddr *) &peer, &peerlen))>=0) {
-			sockflags=fcntl(sock_fd, F_GETFL);
+		sock_fd = accept(master_fd, (struct sockaddr *) &peer, &peerlen);
+		if (sock_fd >= 0) {
+			sockflags = fcntl(sock_fd, F_GETFL);
 			fcntl(sock_fd, F_SETFL, sockflags|O_NONBLOCK);
 			/* Locate a suitable location within the arrays. */
-			if (clients[clients_cnt]!=-1) {
-				for (i=0;i<(sizeof(clients)/sizeof(int));i++) {
-					if (clients[i]==-1) clients_cnt=i;
+			if (clients[clients_cnt] != -1) {
+				for (i = 0; i < (sizeof(clients)/sizeof(int)); i++) {
+					if (clients[i] == -1) {
+						clients_cnt=i;
+					}
 				}
 			}
-			if (clients_cnt<(sizeof(clients)/sizeof(int))) {
-				clients[clients_cnt]=sock_fd;
-				peers[clients_cnt]=peer;
-				clients_http[clients_cnt]=0;
-				if (clients_cnt>clients_cnt_max) clients_cnt_max=clients_cnt;
+			if (clients_cnt < (sizeof(clients)/sizeof(int))) {
+				clients[clients_cnt] = sock_fd;
+				peers[clients_cnt] = peer;
+				clients_http[clients_cnt] = 0;
+				if (clients_cnt > clients_cnt_max) clients_cnt_max = clients_cnt;
 				clients_cnt++;
 				sock_cnt++;
 				LOG_MSG("Accepted new connection from %s", inet_ntoa(peer.sin_addr));
@@ -127,98 +152,108 @@ int main(int argc, char **argv) {
 				close(sock_fd);
 			}
 		}
-		sock_cnt_serv=sock_cnt;
-		for (i=0;i<=clients_cnt_max;i++) {
-			clnt_instr_buf_len[i]=instr_buf_len;
-			errcnt[i]=0;
+		sock_cnt_serv = sock_cnt;
+		for (i = 0;i <= clients_cnt_max; i++) {
+			clnt_instr_buf_len[i] = instr_buf_len;
+			errcnt[i] = 0;
 		}
 
-		backoff_mult_incr=backoff_mult/2;
+		backoff_mult_incr = backoff_mult / 2;
 
 		/* Loop until we've sent 1 block to all the socks or we run out of time for the block. */
 //		for (x=0;x<(_STREAMIT_MAX_BLOCK_WAIT/block_backoff);x++) {
-		for (x=0;x<backoff_mult;x++) {
-			if (sock_cnt_serv==0) break;
+		for (x = 0; x < backoff_mult; x++) {
+			if (sock_cnt_serv == 0) {
+				break;
+			}
 			
-			for (i=0;i<=clients_cnt_max;i++) {
-				if (clnt_instr_buf_len[i]<=0) continue;
-				if (clients[i]==-1) continue;
+			for (i = 0; i <= clients_cnt_max; i++) {
+				if (clnt_instr_buf_len[i] <= 0) {
+					continue;
+				}
+				if (clients[i] == -1) {
+					continue;
+				}
 
 				/* If the client hasn't recieved an HTTP reply yet, don't talk to it. */
-				if (clients_http[i]==0) {
-					write_res=read(clients[i], smbuf, sizeof(smbuf));
-					if (write_res>0) {
+				if (clients_http[i] == 0) {
+					write_res = read(clients[i], smbuf, sizeof(smbuf));
+					if (write_res > 0) {
 						DEBUG_LOG("Client [fd=%i] recieved HTTP response.", clients[i]);
 						write(clients[i], _STREAMIT_HTTP_RESP, strlen(_STREAMIT_HTTP_RESP));
-						clients_http[i]=1;
+						clients_http[i] = 1;
 					}
 
-					clnt_instr_buf_len[i]=0;
-					write_res=0;
+					clnt_instr_buf_len[i] = 0;
+					write_res = 0;
 				} else {
 					/* Otherwise pass it the data */
-					write_res=write(clients[i], buf+(instr_buf_len-clnt_instr_buf_len[i]), clnt_instr_buf_len[i]);
+					write_res = write(clients[i], buf+(instr_buf_len-clnt_instr_buf_len[i]), clnt_instr_buf_len[i]);
 				} 
 
 				/* If we wrote something to the socket, note where we stopped writing. */
-				if (write_res>=0) {
-					clnt_instr_buf_len[i]-=write_res;
-					if (clnt_instr_buf_len[i]==0) sock_cnt_serv--;
+				if (write_res >= 0) {
+					clnt_instr_buf_len[i] -= write_res;
+					if (clnt_instr_buf_len[i] == 0) sock_cnt_serv--;
 				} else {
 					/* If we got an error note it (don't close here, it will be done below) */
-					if (errno==EPIPE || errno==-EPIPE) {
-						clnt_instr_buf_len[i]=-1;
-						sock_cnt_serv-=1;
+					if (errno == EPIPE || errno == -EPIPE) {
+						clnt_instr_buf_len[i] = -1;
+						sock_cnt_serv -= 1;
 					}
 					errcnt[i]++;
 				}
 			}
-			if (sock_cnt_serv==0) break;
+			if (sock_cnt_serv == 0) break;
 			/* If we haven't served atleast 75% of our sockets, we're going too fast, slow down. */
-			percent_served=(((float) (sock_cnt-sock_cnt_serv))/((float) sock_cnt))*100.0;
+			percent_served = (((float) (sock_cnt-sock_cnt_serv))/((float) sock_cnt)) * 100.0;
 			if (percent_served < 75 && x >= (backoff_mult/2)) {
 				DEBUG_LOG("Percent served: %f; sock_cnt=%i, sock_cnt_serv=%i", percent_served, sock_cnt, sock_cnt_serv);
-				block_backoff_total+=500;
-				if (block_backoff_total>_STREAMIT_MAX_BLOCK_WAIT) block_backoff_total=_STREAMIT_MAX_BLOCK_WAIT;
-				block_backoff=block_backoff_total*sock_cnt_serv;
+				block_backoff_total += 500;
+				if (block_backoff_total > _STREAMIT_MAX_BLOCK_WAIT) {
+					block_backoff_total = _STREAMIT_MAX_BLOCK_WAIT;
+				}
+				block_backoff = block_backoff_total * sock_cnt_serv;
 				DEBUG_LOG("Increasing block_backoff=%i", block_backoff);
 			}
 
 //			backoff_mult=(_STREAMIT_MAX_BLOCK_WAIT-(block_backoff*sock_cnt_serv));
-			if (backoff_mult<=4) backoff_mult=5;
-			usleep(block_backoff/sock_cnt_serv);
+			if (backoff_mult <= 4) {
+				backoff_mult = 5;
+			}
+			usleep(block_backoff / sock_cnt_serv);
 		}
 
 		/* If we haven't served atleast 75% of our sockets, we're going too fast, slow down. */
-		percent_served=(((float) (sock_cnt-sock_cnt_serv))/((float) sock_cnt))*100.0;
+		percent_served = (((float) (sock_cnt-sock_cnt_serv))/((float) sock_cnt))*100.0;
 		if (percent_served < 75) {
 			DEBUG_LOG("Percent served: %f; sock_cnt=%i, sock_cnt_serv=%i", percent_served, sock_cnt, sock_cnt_serv);
-			backoff_mult+=backoff_mult_incr;
+			backoff_mult += backoff_mult_incr;
 			DEBUG_LOG("Increasing backoff_mult=%i", backoff_mult);
 		}
 
 		/* Kill sockets who were unable to keep up. */
-		for (i=0;i<=clients_cnt_max;i++) {
-			if (clnt_instr_buf_len[i]!=0) {
-				if (clients[i]==-1) continue;
+		for (i = 0; i <= clients_cnt_max; i++) {
+			if (clnt_instr_buf_len[i] != 0) {
+				if (clients[i] == -1) continue;
 //				LOG_MSG("Closing client, write_res=%i, fd=%i, i=%i, errno=%i: ", write_res, clients[i], i, errno);
 //				perror("write");
-				if (errno==11) {
+				if (errno == 11) {
 					/* Don't kill slow HTTP clients. */
-					if (clients_http[i]==0) continue;
+					if (clients_http[i] == 0) continue;
 					LOG_MSG("Closing connection from %s, client unable to keep up.", inet_ntoa(peers[i].sin_addr));
 				} else {
 					LOG_MSG("Closing connection from %s", inet_ntoa(peers[i].sin_addr));
 				}
 				close(clients[i]);
-				clients[i]=-1;
-				clients_http[i]=0;
-				clients_cnt=i;
+				clients[i] = -1;
+				clients_http[i] = 0;
+				clients_cnt = i;
 				sock_cnt--;
 			}
 		}
 /* This will be replaced with a dynamic wait from tv.usec */
-		if (sock_cnt==0) {
+		if (sock_cnt == 0) {
 			usleep(700000);
 		} else {
 			usleep(5000);
